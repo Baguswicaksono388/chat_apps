e72b6fa6dece1184a28c1601a33d61569e313d72
Bagus <sgiapi1@gmail.com>
2021-04-17 10:00:31 +0700

    Bagus [Dev] Preparation file server.js

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
+const io = socketio(server);
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
