c1bf62285a1882383dd913f11347a7d31948fa25
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-04-20 08:22:47 +0700

    Bagus [Dev] Menambahkan file helper di server

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const socketio = require('socket.io');
+const PORT = process.env.PORT || 5000;
+const { addUser } = require('./helper');
+
 const io = socketio(server, {
   cors: {
     origin: '*',
   }
 });
-const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -19,7 +21,19 @@ io.on('connection', (socket) => {
   socket.on('create-room', name=>{
     console.log('Then room name received is ', name);
   });
-
+  socket.on('join', ({ name, room_id, user_id }) => {
+    const { error, user } = addUser({
+      socket_id: socket.id,
+      name,
+      room_id,
+      user_id
+    });
+    if (error) {
+      console.log('join error', error)
+    } else {
+      console.log('join user', user)
+    }
+  })
   socket.on("ping", function (inData) {
     console.log("ping received");
     socket.emit("pong", { });
